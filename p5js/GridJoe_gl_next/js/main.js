ebac8a8e24bad4ac85c6a2e3522efc6011d81c04
Nick Fox-Gieg <nick@fox-gieg.com>
2026-03-15 01:56:44 -0400

    cleanup

--- p5js/GridJoe_gl_next/js/main.js
+++ p5js/GridJoe_gl_next/js/main.js
@@ -1,264 +1,197 @@
 "use strict";
 
 // ---     MAIN CONTROLS     ---
-// if you want to avoid chain reactions, try 0, 20, 100, 0.2
-let delayCounter = 0;    // int, delays start of spread
-let lifeCounter = 20;    // int, how long spread lasts
-let respawnCounter = 50; // int, how long until retrigger
-let globalChaos = 0.3;    // float, 0 = min, 1 = max
+let delayCounter = 5; //0;
+let lifeCounter = 40; //20;
+let respawnCounter = 50; //50;
+let globalChaos = 0.3; //0.3;
 // -------------------------
-let choose = 0;    // int
-let maxChoices = 7;    // int
-let numFrames = 50;    // int
-let renderCounterMax = 1000;    // int
-// ----
-let pixelSize = 5;    // int
-let sW = 640;    // int
-let sH = 480;    // int
-let fps = 60;    // int
+let choose = 0;
+let maxChoices = 7;
+let sW = 640;
+let sH = 480;
+let fps = 60;
 
-let numColumns, numRows;    // int
-let guyWidth, guyHeight, startX, startY;    // float
-let mainGrid = [];  // GridGuy[][] 
-let setRules = "";    // string
-let odds_X_Yplus1, odds_Xminus1_Y, odds_X_Yminus1, odds_Xplus1_Y, odds_Xplus1_Yplus1, odds_Xminus1_YminuX1, odds_Xplus1_Yminus1, odds_Xminus1_Yplus1;    // float
+// Shader resources
+let simulationShader;
+let renderShader;
 
-let target;    // Target
+// p5.js Framebuffers for ping-pong
+let fboA, fboB;
+let currentBuffer = 0;
+
+// Target (autonomous cursor)
+let target;
+
+// Propagation odds: NW, N, NE, W, E, SW, S, SE
+let odds = [0, 0.5, 0, 0.5, 0.5, 0, 0.5, 0];
+
+// Pattern presets
+const patterns = [
+    { odds: [0, 0.5, 0, 0.5, 0.5, 0, 0.5, 0], randomize: [] },
+    { odds: [null, 1, 1, null, 0, 1, 0, null], randomize: [0, 3, 7] },
+    { odds: [0, 0, null, null, null, null, 0, 0], randomize: [2, 3, 4, 5], scale: [1, 1, 0.1, 1, 1, 0.1, 1, 1] },
+    { odds: [0, 0.1, 0, 0, 0, null, 0.5, null], randomize: [5, 7] },
+    { odds: [0, 0, 0, 0, 0, null, 1, null], randomize: [5, 7], scale: [1, 1, 1, 1, 1, 0.1, 1, 0.1] },
+    { odds: [null, 1, null, 0, 0, 0, 0, 0], randomize: [0, 2], scale: [0.1, 1, 0.1, 1, 1, 1, 1, 1] },
+    { odds: [null, null, null, null, null, null, null, null], randomize: [0,1,2,3,4,5,6,7] }
+];
+
+function preload() {
+    simulationShader = loadShader('shaders/passthrough.vert', 'shaders/simulation.frag');
+    renderShader = loadShader('shaders/passthrough.vert', 'shaders/render.frag');
+}
 
 function setup() {
     createCanvas(sW, sH, WEBGL);
-    
+    pixelDensity(1);
     noCursor();
-
     frameRate(fps);
+    noStroke();
 
-    pixelOddsSetup();
-    initGlobals();
-    
-    for (let y = 0; y < numRows; y++) {
-        for (let x = 0; x < numColumns; x++) {
-            rulesInit(x, y);
-            guysInit(x, y);
+    // Create framebuffers using p5.js API (available in p5.js 1.7+)
+    let fboSettings = {
+        width: sW,
+        height: sH,
+        density: 1,
+        textureFiltering: NEAREST,
+        antialias: false,
+        depth: false
+    };
+    fboA = createFramebuffer(fboSettings);
+    fboB = createFramebuffer(fboSettings);
+
+    // Clear both
+    fboA.begin();
+    clear();
+    fboA.end();
+    fboB.begin();
+    clear();
+    fboB.end();
+
+    target = new Target();
+    setupPattern();
+
+    console.log("GridJoe GPU - p5.js Framebuffer ping-pong initialized");
+}
+
+function setupPattern() {
+    choose = int(random(maxChoices));
+    console.log("Pattern:", choose);
+
+    let pattern = patterns[choose];
+    for (let i = 0; i < 8; i++) {
+        if (pattern.odds[i] === null || pattern.randomize.includes(i)) {
+            let val = random(1);
+            if (pattern.scale && pattern.scale[i]) {
+                val *= pattern.scale[i];
+            }
+            odds[i] = val;
+        } else {
+            odds[i] = pattern.odds[i];
         }
     }
-    
-    target = new Target();    
 }
 
 function draw() {
     target.run();
     if (target.armResetAll) {
-        resetAll();
+        //resetAll();
+        setupPattern(); // Only change pattern, keep animation
         target.armResetAll = false;
     }
-    
-    background(0);
-    
-    for (let y = 0; y < numRows; y++) {
-        for (let x = 0; x < numColumns; x++) {
-            //let loc = x + (y * numColumns);
 
-            rulesHandler(x, y);
-            mainGrid[x][y].run();
-        }
-    }  
+    let readFBO = currentBuffer === 0 ? fboA : fboB;
+    let writeFBO = currentBuffer === 0 ? fboB : fboA;
+
+    // --- SIMULATION PASS: render to framebuffer ---
+    writeFBO.begin();
+    clear();
+    shader(simulationShader);
+    simulationShader.setUniform('u_state', readFBO.color);
+    simulationShader.setUniform('u_resolution', [sW, sH]);
+    simulationShader.setUniform('u_target', [target.posX, -target.posY]);
+    simulationShader.setUniform('u_targetClicked', target.clicked ? 1.0 : 0.0);
+    simulationShader.setUniform('u_time', millis() / 1000.0);
+    simulationShader.setUniform('u_deltaTime', deltaTime / 1000.0);
+    simulationShader.setUniform('u_oddsNW', odds[0]);
+    simulationShader.setUniform('u_oddsN', odds[1]);
+    simulationShader.setUniform('u_oddsNE', odds[2]);
+    simulationShader.setUniform('u_oddsW', odds[3]);
+    simulationShader.setUniform('u_oddsE', odds[4]);
+    simulationShader.setUniform('u_oddsSW', odds[5]);
+    simulationShader.setUniform('u_oddsS', odds[6]);
+    simulationShader.setUniform('u_oddsSE', odds[7]);
+    simulationShader.setUniform('u_delayFrames', delayCounter);
+    simulationShader.setUniform('u_lifeFrames', lifeCounter);
+    simulationShader.setUniform('u_respawnFrames', respawnCounter);
+    simulationShader.setUniform('u_chaos', globalChaos);
+    rect(-sW/2, -sH/2, sW, sH);
+    writeFBO.end();
+
+    // Swap buffers
+    currentBuffer = 1 - currentBuffer;
+
+    // --- RENDER PASS: render to screen ---
+    clear();
+    shader(renderShader);
+    renderShader.setUniform('u_state', writeFBO.color);
+    renderShader.setUniform('u_resolution', [sW, sH]);
+    renderShader.setUniform('u_time', millis() / 1000.0);
+    rect(-sW/2, -sH/2, sW, sH);
+    resetShader();
 }
 
 function keyPressed() {
     resetAll();
 }
 
-function initGlobals() {
-    numColumns = sW / pixelSize;
-    numRows = sH / pixelSize;
-
-    guyWidth = sW / numColumns;
-    guyHeight = sH / numRows;
-
-    startX = guyWidth / 2;
-    startY = guyHeight / 2;
-
-    // make mainGrid a 2D array
-    for (let i = 0; i < numColumns; i++) {
-        let mg = [];
-        for (let j = 0; j < numRows; j++) {
-            let g = new GridGuy(startX, startY, guyWidth, guyHeight, setRules, globalChaos, delayCounter, lifeCounter, respawnCounter);
-            mg.push(g);
-        }
-        mainGrid.push(mg);
-    }
-}
-
-function rulesHandler(x, y) {  // int, int
-    let sw = mainGrid[x][y].switchArray;  // bool[]
-    if (sw[0] || sw[1] || sw[2] || sw[3] || sw[4] || sw[5] || sw[6] || sw[7]) return;
-
-    if (mainGrid[x][y].clicked) {
-        //these are direction probabilities
-        mainGrid[x][y + 1].kaboom = diceHandler(1, odds_X_Yplus1);
-        mainGrid[x - 1][y].kaboom = diceHandler(1, odds_Xminus1_Y);
-        mainGrid[x][y - 1].kaboom = diceHandler(1, odds_X_Yminus1);
-        mainGrid[x + 1][y].kaboom = diceHandler(1, odds_Xplus1_Y);
-        mainGrid[x + 1][y + 1].kaboom = diceHandler(1, odds_Xplus1_Yplus1);
-        mainGrid[x - 1][y - 1].kaboom = diceHandler(1, odds_Xminus1_YminuX1);
-        mainGrid[x + 1][y - 1].kaboom = diceHandler(1, odds_Xplus1_Yminus1);
-        mainGrid[x - 1][y + 1].kaboom = diceHandler(1, odds_Xminus1_Yplus1);
-    }
-}
-
-function diceHandler(v1, v2) {  // int, int
-    let rollDice = random(v1);  // float
-    return rollDice < v2;
-}
-
-function rulesInit(x, y) {  // int, int
-    setRules = "";
-    if (x == 0 && y == 0) {
-        setRules = "NWcorner";
-    } else if (x == numColumns - 1 && y == 0) {
-        setRules = "NEcorner";
-    } else if (x == 0 && y == numRows - 1) {
-        setRules = "SWcorner";
-    } else if (x == numColumns - 1 && y == numRows - 1) {
-        setRules = "SEcorner";
-    } else if (y == 0) {
-        setRules = "Nrow";
-    } else if (y == numRows - 1) {
-        setRules = "Srow";
-    } else if (x == 0) {
-        setRules = "Wrow";
-    } else if (x == numColumns - 1) {
-        setRules = "Erow";
-    }
-}
-
-function guysInit(x, y) {  // int, int
-    mainGrid[x][y] = new GridGuy(startX, startY, guyWidth, guyHeight, setRules, globalChaos, delayCounter, lifeCounter, respawnCounter);
-    if (startX < width - guyWidth) {
-        startX += guyWidth;
-    } else {
-        startX = guyWidth / 2;
-        startY += guyHeight;
-    }
-    console.log("init " + x + " " + y);
-}
-
 function resetAll() {
-    startX = 0;
-    startY = 0;
-    //currentFrame = 0;
-    for (let y = 0; y < numRows; y++) {
-        for (let x = 0; x < numColumns; x++) {
-            mainGrid[x][y].hovered = false;
-            mainGrid[x][y].clicked = false;
-            //mainGrid[x][y].kaboom = false;
-            mainGrid[x][y].delayCountDown = mainGrid[x][y].delayCountDownOrig;
-            mainGrid[x][y].lifeCountDown = mainGrid[x][y].lifeCountDownOrig;
-            mainGrid[x][y].respawnCountDown = mainGrid[x][y].respawnCountDownOrig;
-            mainGrid[x][y].fillColor = mainGrid[x][y].fillColorOrig;
-        }
-    }
-    
-    pixelOddsSetup();
+    fboA.begin();
+    clear();
+    fboA.end();
+    fboB.begin();
+    clear();
+    fboB.end();
+    setupPattern();
 }
 
-// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
-
-let randomValues = new Array(8);  // float[]
-
-function pixelOddsSetup() {
-    // temp
-    for (let i = 0; i < randomValues.length; i++) {
-        randomValues[i] = random(1);
+// Target class
+class Target {
+    constructor() {
+        this.speedMin = 0.01;
+        this.speedMax = 0.05;
+        this.speed = 0.03;
+        this.clickOdds = 0.1;
+        this.chooseOdds = 0.01;
+        this.markTime = 0;
+        this.timeInterval = 200;
+        this.posX = 0;
+        this.posY = 0;
+        this.targetX = 0;
+        this.targetY = 0;
+        this.minDist = 5;
+        this.clicked = false;
+        this.armResetAll = false;
+        this.pickTarget();
     }
 
-    choose = int(random(maxChoices));
-    console.log("choose: " + choose);
-    
-    switch (choose) {
-    case 0: 
-        // 0. CROSS | OK
-        //delayCounter = 4;
-        odds_Xminus1_YminuX1 = 0;//randomValues[0]; // x-1 y-1
-        odds_X_Yminus1 = 0.5;//randomValues[1]; // x y-1
-        odds_Xplus1_Yminus1 = 0;//randomValues[2]; // x+1 y-1
-        odds_Xminus1_Y = 0.5;//randomValues[3]; // x-1 y
-        odds_Xplus1_Y = 0.5;//randomValues[4]; // x+1 y
-        odds_Xminus1_Yplus1 = 0;//randomValues[5]; // x-1 y+1
-        odds_X_Yplus1 = 0.5;//randomValues[6]; // x y+1
-        odds_Xplus1_Yplus1 = 0;//randomValues[7]; // x+1 y+1        
-        break;
-    case 1:
-        // 1. WOT | ?
-        //delayCounter = 4;
-        odds_Xminus1_YminuX1 = randomValues[0]; // x-1 y-1
-        odds_X_Yminus1 = 1;//randomValues[1]; // x y-1
-        odds_Xplus1_Yminus1 = 1;//randomValues[2]; // x+1 y-1
-        odds_Xminus1_Y = randomValues[3]; // x-1 y
-        odds_Xplus1_Y = 0;//randomValues[4]; // x+1 y
-        odds_Xminus1_Yplus1 = 1;//randomValues[5]; // x-1 y+1
-        odds_X_Yplus1 = 0;//randomValues[6]; // x y+1
-        odds_Xplus1_Yplus1 = randomValues[7]; // x+1 y+1
-        break;
-    case 2: 
-        // 2. OCEAN | OK
-        //delayCounter = 4;
-        odds_Xminus1_YminuX1 = 0;//randomValues[0]; // x-1 y-1
-        odds_X_Yminus1 = 0;//randomValues[1]; // x y-1
-        odds_Xplus1_Yminus1 = 0.1 * randomValues[2]; // x+1 y-1
-        odds_Xminus1_Y = randomValues[3]; // x-1 y
-        odds_Xplus1_Y = randomValues[4]; // x+1 y
-        odds_Xminus1_Yplus1 = 0.1 * randomValues[5]; // x-1 y+1
-        odds_X_Yplus1 = 0;//randomValues[6]; // x y+1
-        odds_Xplus1_Yplus1 = 0;//randomValues[7]; // x+1 y+1 
-        break;
-    case 3: 
-        // 3. MOUNTAINS
-        //delayCounter = 4;
-        odds_Xminus1_YminuX1 = 0;//randomValues[0]; // x-1 y-1
-        odds_X_Yminus1 = 0.1;//randomValues[1]; // x y-1
-        odds_Xplus1_Yminus1 = 0;//randomValues[2]; // x+1 y-1
-        odds_Xminus1_Y = 0;//randomValues[3]; // x-1 y
-        odds_Xplus1_Y = 0;//randomValues[4]; // x+1 y
-        odds_Xminus1_Yplus1 = randomValues[5]; // x-1 y+1
-        odds_X_Yplus1 = 0.5;//randomValues[6]; // x y+1
-        odds_Xplus1_Yplus1 = randomValues[7]; // x+1 y+1    
-        break;
-    case 4: 
-        // 4. DROPS
-        //delayCounter = 4;
-        odds_Xminus1_YminuX1 = 0;//randomValues[0]; // x-1 y-1
-        odds_X_Yminus1 = 0;//randomValues[1]; // x y-1
-        odds_Xplus1_Yminus1 = 0;//randomValues[2]; // x+1 y-1
-        odds_Xminus1_Y = 0;//randomValues[3]; // x-1 y
-        odds_Xplus1_Y = 0;//randomValues[4]; // x+1 y
-        odds_Xminus1_Yplus1 = 0.1 * randomValues[5]; // x-1 y+1
-        odds_X_Yplus1 = 1;//randomValues[6]; // x y+1
-        odds_Xplus1_Yplus1 = 0.1 * randomValues[7]; // x+1 y+1    
-        break;
-    case 5: 
-        // 5. DROPS_REVERSE
-        //delayCounter = 4;
-        odds_Xminus1_YminuX1 = 0.1 * randomValues[0]; // x-1 y-1
-        odds_X_Yminus1 = 1;//randomValues[1]; // x y-1
-        odds_Xplus1_Yminus1 = 0.1 * randomValues[2]; // x+1 y-1
-        odds_Xminus1_Y = 0;//randomValues[3]; // x-1 y
-        odds_Xplus1_Y = 0;//randomValues[4]; // x+1 y
-        odds_Xminus1_Yplus1 = 0; //randomValues[5]; // x-1 y+1
-        odds_X_Yplus1 = 0;//randomValues[6]; // x y+1
-        odds_Xplus1_Yplus1 = 0; //randomValues[7]; // x+1 y+1    
-        break;
-    case 6:
-        // 6. ALL RANDOM
-        //delayCounter = 4;
-        odds_Xminus1_YminuX1 = randomValues[0]; // x-1 y-1
-        odds_X_Yminus1 = randomValues[1]; // x y-1
-        odds_Xplus1_Yminus1 = randomValues[2]; // x+1 y-1
-        odds_Xminus1_Y = randomValues[3]; // x-1 y
-        odds_Xplus1_Y = randomValues[4]; // x+1 y
-        odds_Xminus1_Yplus1 = randomValues[5]; // x-1 y+1
-        odds_X_Yplus1 = randomValues[6]; // x y+1
-        odds_Xplus1_Yplus1 = randomValues[7]; // x+1 y+1
-        break;
+    run() {
+        this.posX = lerp(this.posX, this.targetX, this.speed);
+        this.posY = lerp(this.posY, this.targetY, this.speed);
+        if (millis() > this.markTime + this.timeInterval ||
+            dist(this.posX, this.posY, this.targetX, this.targetY) < this.minDist) {
+            this.pickTarget();
+        }
+    }
+
+    pickTarget() {
+        this.markTime = millis();
+        this.targetX = lerp(this.posX, random(-sW/2, sW/2), 0.5);
+        this.targetY = lerp(this.posY, random(-sH/2, sH/2), 0.5);
+        this.speed = random(this.speedMin, this.speedMax);
+        let r = random(1);
+        if (r < this.clickOdds) this.clicked = !this.clicked;
+        if (r < this.chooseOdds) this.armResetAll = true;
     }
 }
